1c2e6f91fef049388e6ec8a57bb1eeeb2dda3a32
Jennifer Uvina <Jennifer.Uvina@blueye.com>
2016-12-14 09:09:01 -0600

    Updates to inline comments

--- 01-drums/main.js
+++ 01-drums/main.js
@@ -1,17 +1,16 @@
 function playSound(e){
-  //find associated audio tag by keyCode/data-key attributes, uses ES6 template literals/strings
+  //uses ES6 template literals/strings
+  //find associated audio tag by keyCode/data-key attributes
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   //find associated key div for visual effects
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
   //escape if no element associated with keyCode, else play sound.
   if(!audio){ return; } 
-  
   //reset sound for repeated key presses
   audio.currentTime = 0;
   audio.play();
   key.classList.add('playing');
-
 }
 
 function removeTransition(e){
